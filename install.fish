94858ba5840eb2fe8575e1b825ba3035045bfef4
Andre Luiz dos Santos <andre@a.fastville.com.br>
2016-01-13 16:59:06 -0200

    Minor modifications, no change in behaviour. Convert to tabs, use long option.

--- install.fish
+++ install.fish
@@ -8,10 +8,10 @@ and sed --in-place=.z_backup --expression=$script $cf
 or exit
 
 begin
-  echo '### Z ###'
-  echo '### https://github.com/andre-luiz-dos-santos/z'
-  echo 'if status --is-interactive'
-  echo '  source '(dirname (status --current-filename))'/z.fish'
-  echo 'end'
-  echo '### Z ###'
+	echo '### Z ###'
+	echo '### https://github.com/andre-luiz-dos-santos/z'
+	echo 'if status --is-interactive'
+	echo '	source '(dirname (status --current-filename))'/z.fish'
+	echo 'end'
+	echo '### Z ###'
 end >> $cf
